85d57e39f22438b42344964c75c941f5d52f31b6
megh21 <meghrajvbagde@gmail.com>
2024-11-16 00:46:15 +0100

    Remove commented dimensions parameter for Cohere embeddings in PDFProcessor

--- build.ps1
+++ build.ps1
@@ -7,11 +7,3 @@ Set-Location "D:\project\chat_federated"
 
 # Build the Docker image
 docker build -t ${imageName}:${tag} .
-
-# # Log in to Docker Hub using environment variables
-# docker login --username mbnextsolutions --password madman213
-# # Push the Docker image to Docker Hub
-# docker push ${imageName}:${tag}
-
-# # Log out from Docker Hub
-# docker logout
